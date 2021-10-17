f4e6ce82788ecce7d59ec690d84f290c6ed5d8b2
Manami Ueda <manamiueda1@gmail.com>
2021-10-17 15:07:44 -0400

    completed arrow functions section

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-console.log('Hello TypeScript!');
+console.log('Ahoy TypeScript!');
+
+// arrow function vs implicit return
+
+// const pizzas = [{ name: 'pepperoni', toppings: ['pepperoni'] }];
+
+// const mappedPizzas = pizzas.map((pizza, index) => {
+//   return pizza.name.toUpperCase();
+// });
+
+// console.log(mappedPizzas)
+
+// the arrow function inside getName ignores the scope -> it's not binding the 'this' value
+const pizza: any = {
+  name: 'blazing inferno',
+  getName: function () {
+    console.log(this.name)
+  }
+}
+
+console.log(pizza.getName())
