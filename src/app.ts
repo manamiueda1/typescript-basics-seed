404967c8094ef2f5c107979ff4260b56f279a50f
Manami Ueda <manamiueda1@gmail.com>
2021-10-20 17:39:03 -0400

    class inheritance

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,3 @@
-console.log('Ahoy TypeScript!');
-
 // arrow function vs implicit return
 
 // const pizzas = [{ name: 'pepperoni', toppings: ['pepperoni'] }];
@@ -11,11 +9,55 @@ console.log('Ahoy TypeScript!');
 // console.log(mappedPizzas)
 
 // the arrow function inside getName ignores the scope -> it's not binding the 'this' value
-const pizza: any = {
-  name: 'blazing inferno',
-  getName: function () {
-    console.log(this.name)
+// const pizza: any = {
+//   name: 'blazing inferno',
+//   getName: function () {
+//     console.log(this.name)
+//   }
+// }
+
+// console.log(pizza.getName())
+
+// const coupon: string = 'PIZZA25'
+
+// function normalizeCoupon(code: string): string {
+//   return code.toUpperCase()
+// }
+
+// const couponMessage: string = `final coupon is ${normalizeCoupon(coupon)}`
+// console.log(couponMessage)
+// console.log(normalizeCoupon(coupon))
+
+// class example
+
+
+abstract class Sizes {
+  constructor(public sizes: string[]) { }
+  
+  set availableSizes(sizes: string[]) {
+    this.sizes = sizes;
+  }
+
+  get availableSizes() {
+    return this.sizes;
   }
 }
 
-console.log(pizza.getName())
+class Pizza extends Sizes{
+  
+  toppings: string[] = [];
+
+  constructor(public name: string, public sizes: string[]) {
+    super(sizes)
+  }
+  
+  addTopping(topping: string) {
+    this.toppings.push(topping)
+    console.log(this.name, 'pizza name')
+  }
+}
+
+const pizza = new Pizza('Pepperoni', ['small', 'medium'])
+pizza.addTopping('cheese')
+console.log(pizza.availableSizes)
+console.log(pizza)
